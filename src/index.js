53bb3e45d336062bb18b925ac606e942d1a31f35
hmDev27 <hmdev27@gmail.com>
2025-08-29 00:32:09 +0630

    backend ready for deploy

--- src/index.js
+++ src/index.js
@@ -21,7 +21,8 @@ app.use(express.json({ limit: "10mb" }));
 app.use(express.urlencoded({ limit: "10mb", extended: true }));
 app.use(cookieParser());
 app.use(cors({
-    origin: "http://192.168.124.2:5173",
+    origin: "*",
+,
     credentials: true,
 })
 )
@@ -29,15 +30,7 @@ app.use(cors({
 app.use("/api/auth/", authRoutes);
 app.use("/api/messages", messageRoutes);
 
-if(process.env.NODE_ENV==="production"){
-    app.use(express.static(path.join(__dirname, "../frontend/dist")));
 
-
-    
-app.get("*", (req, res) => {
-    res.sendFile(path.join(__dirname, "../frontend", "dist", "index.html"));
-})
-}
 
 server.listen(process.env.PORT,'0.0.0.0', ()=> {
     console.log("Server is running on PORT:", PORT);
