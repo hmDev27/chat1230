bf126567a093ba0e0c34704eeb449525f676f139
hmDev27 <hmdev27@gmail.com>
2025-08-29 12:06:17 +0630

    Fix CORS config and finalize backend routes

--- src/index.js
+++ src/index.js
@@ -21,11 +21,13 @@ app.use(express.json({ limit: "10mb" }));
 app.use(express.urlencoded({ limit: "10mb", extended: true }));
 app.use(cookieParser());
 app.use(cors({
-    origin: "https://chat1031.netlify.app",
+  origin: "https://chat1031.netlify.app",
+  credentials: true,
+  methods: ["GET", "POST", "PUT", "DELETE"],
+  allowedHeaders: ["Content-Type", "Authorization"],
+}))
 
-    credentials: true,
-})
-)
+
 
 app.use("/api/auth/", authRoutes);
 app.use("/api/messages", messageRoutes);
